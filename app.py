43751d1f34d6a5f033097b2b036c41198800fd67
wuxjian <wuxj@wbpharma.com>
2023-06-14 10:31:37 +0800

    优化代码结构

---
+++ app.py
@@ -0,0 +1,7 @@
+import gradio as gr
+from scripts.common import build_ui
+
+ui = build_ui()
+
+ui.launch()
+
--- scripts/setup.py
+++ scripts/setup.py
@@ -1,44 +1,17 @@
+import imp
 import gradio as gr
-import json
-from os import path
 import modules.scripts as scripts
 from modules import script_callbacks
 
+from common import build_ui
 
-def buildTab(tab_name: str, d: dict):
-    with gr.Tab(tab_name, elem_id=f"promot-tab-{tab_name}"):
-        l = []
-        for (k, v) in d.items():
-            l.append(k + "-" + v)
-        gr.CheckboxGroup(l, show_label=False, elem_classes="promot-group", elem_id=f"promot-group-{tab_name}")
-        
 
-def buildTabs(type: str):
-    for (k, v) in json_data[type].items():
-        buildTab(k, v)
-
-### 获取数据
-with open(path.join(path.dirname(path.abspath(__file__)), 'data.json'), encoding='utf-8') as f:
-    json_data = json.loads(f.read())
+app_name = '提示词'
 
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
-        with gr.Blocks() as app:
-            gr.Markdown("#### 提示词信息")
-            with gr.Row():
-                gr.Textbox(label="正面", lines=3, elem_id="positive_word_textbox")
-                # positive_button = gr.Button("复制")
-            with gr.Row():
-                gr.Textbox(label="负面", lines=3, elem_id="positive_word_textbox")
-
-            gr.Markdown("#### 选择提示词信息")
-
-            with gr.Blocks():
-                gr.Markdown('##### 选择正面提示词信息')
-                buildTabs('positive')
-
-
-        return [(ui_component, "提示词", "提示词")]
+        build_ui()
+        return [(ui_component, app_name, app_name)]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
 
