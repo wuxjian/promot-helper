e9f30f6ca46c26f1791a6f54e32dc41882c06b4d
wuxjian <wuxj@wbpharma.com>
2023-06-14 10:59:39 +0800

    修改布局

--- scripts/common.py
+++ scripts/common.py
@@ -11,9 +11,10 @@ def buildTab(tab_name: str, d: dict):
         gr.CheckboxGroup(l, show_label=False, elem_classes="promot-group", elem_id=f"promot-group-{tab_name}")
         
 
-def buildTabs(type: str):
-    for (k, v) in json_data[type].items():
-        buildTab(k, v)
+def buildTabs(type: str, label: str):
+    with gr.Accordion(label, open=True, elem_id=f"{type}_accordion"):
+        for (k, v) in json_data[type].items():
+            buildTab(k, v)
 
 ### 获取数据
 with open(path.join(path.dirname(path.abspath(__file__)), 'data.json'), encoding='utf-8') as f:
@@ -23,16 +24,23 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'data.json'), encoding
 def build_ui():
     with gr.Blocks() as ui:
         gr.Markdown("#### 提示词信息")
-        with gr.Row():
+        with gr.Column():
             gr.Textbox(label="正面", lines=3, elem_id="positive_word_textbox")
-            # positive_button = gr.Button("复制")
-        with gr.Row():
-            gr.Textbox(label="负面", lines=3, elem_id="positive_word_textbox")
-
-        gr.Markdown("#### 选择提示词信息")
+            with gr.Row():
+                gr.Button("发送到文生图", elem_id='positive-send-txt2img')
+                gr.Button("发送到图生图", elem_id='positive-send-img2img')
+                gr.Button("清空", elem_id='positive-clear')
+        with gr.Column():
+            gr.Textbox(label="负面", lines=3, elem_id="negative_word_textbox")
+            with gr.Row():
+                gr.Button("发送到文生图", elem_id='negative-send-txt2img')
+                gr.Button("发送到图生图", elem_id='negative-send-img2img')
+                gr.Button("清空", elem_id='negative-clear')
 
         with gr.Blocks():
-            gr.Markdown('##### 选择正面提示词信息')
-            buildTabs('positive')
+            buildTabs('positive', '正面提示词')
         
+        with gr.Blocks():
+            buildTabs('negative', '负面提示词')
+
         return ui
